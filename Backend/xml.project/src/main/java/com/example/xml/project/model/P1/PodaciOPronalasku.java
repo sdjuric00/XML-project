ab4812b524de2f57dd3669a57526f782d65b8f44
anestejzn <anastasijas12@gmail.com>
2022-12-08 16:40:29 +0100

    P1 unmarshall

---
+++ Backend/xml.project/src/main/java/com/example/xml/project/model/P1/PodaciOPronalasku.java
@@ -0,0 +1,18 @@
+package com.example.xml.project.model.P1;
+
+import com.example.xml.project.model.A1.Autor;
+
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name="zahtev_za_priznavanje_patenta",
+        propOrder={"nazivi"},
+        namespace = "http://ftn.ac.rs/a"
+)
+public class PodaciOPronalasku {
+    @XmlElementWrapper(name="nazivi", namespace = "http://ftn.ac.rs/a")
+    @XmlElement(name="naziv", namespace = "http://ftn.ac.rs/a")
+    public List<Naziv> nazivi = new ArrayList<>();
+
+}
