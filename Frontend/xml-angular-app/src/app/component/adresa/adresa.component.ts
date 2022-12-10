14b2caf16777015bee03fd5dc7451bd1ee8e1d26
sdjuric00 <103071405+sdjuric00@users.noreply.github.com>
2022-12-10 13:27:29 +0100

    Adding autocomplete for countries

--- Frontend/xml-angular-app/src/app/component/adresa/adresa.component.ts
+++ Frontend/xml-angular-app/src/app/component/adresa/adresa.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ControlContainer, FormGroup} from "@angular/forms";
+import { map, Observable, startWith } from 'rxjs';
+import { Country } from 'src/app/model/country';
+import { countries } from 'src/app/model/country-data';
 
 @Component({
   selector: 'app-adresa',
@@ -9,12 +12,27 @@ import {ControlContainer, FormGroup} from "@angular/forms";
 export class AdresaComponent implements OnInit {
   public podnosilacFormGroup: FormGroup;
 
+  public countries: Country[] = countries;
+  public filteredCountries: Observable<Country[]> | undefined;
+
   constructor(private controlContainer: ControlContainer) {
     this.podnosilacFormGroup = <FormGroup>this.controlContainer.control;
   }
 
   ngOnInit(): void {
     this.podnosilacFormGroup = <FormGroup>this.controlContainer.control;
+    if (this.podnosilacFormGroup) {
+      this.filteredCountries = this.podnosilacFormGroup.get('drzava')?.valueChanges.pipe(
+          startWith(''),
+          map((country: string) => (country ? this._filterCountries(country) : this.countries.slice()))
+        );
+    }
+  }
+
+  _filterCountries(value: string): Country[] {
+    const filterValue = value.toLowerCase();
+
+    return this.countries.filter(country => country.name.toLowerCase().includes(filterValue));
   }
 
 }
